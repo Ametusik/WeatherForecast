1f822a5ef3c262353fe59ee9a52e08d72d8b69c2
Amet Kurtumerov <leonor340@mail.ru>
2024-07-24 18:56:00 +0300

    Реализация корректного автозаполнения

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,26 +1,42 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormsModule} from "@angular/forms";
 import {DadataService} from "../../shared/services/dadata.service";
 import {NgForOf} from "@angular/common";
+import {Suggestion} from "../../shared/entities/suggestion";
+import {MatAutocomplete, MatAutocompleteTrigger, MatOption} from "@angular/material/autocomplete";
+import {MatInput} from "@angular/material/input";
 
 @Component({
   selector: 'app-search',
   standalone: true,
   imports: [
     FormsModule,
-    NgForOf
+    NgForOf,
+    MatAutocomplete,
+    MatOption,
+    MatAutocompleteTrigger,
+    MatInput
   ],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss'
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
   searchQuery: string;
-  suggestions:any[];
-  constructor(private dadataService:DadataService) {
+  suggestions: Suggestion[];
+
+  constructor(private dadataService: DadataService) {
 
   }
 
+  ngOnInit() {
+    this.dadataService.getSuggestions(this.searchQuery)
+      .subscribe(data => {
+        this.suggestions = data.suggestions;
+      });
+  }
+
   getSuggestions(): void {
+    this.suggestions.length = 0;
     this.dadataService.getSuggestions(this.searchQuery)
       .subscribe(data => {
         this.suggestions = data.suggestions;
